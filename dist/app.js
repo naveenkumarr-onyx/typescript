2472e839f28ed9b8370dee8b18f05e0572dfc269
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-12-30 17:40:40 +0530

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+require("dotenv/config");
+const cors_1 = __importDefault(require("cors"));
+const db_1 = require("./config/db");
 const app = (0, express_1.default)();
 app.get("/", (req, res) => {
-    res.send("Hello Wo");
+    res.send(`Backend is running on port ${process.env.APP_PORT}... Have fun!`);
 });
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+(0, db_1.connectDB)();
+app.use((0, cors_1.default)({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.listen(process.env.APP_PORT, () => {
+    console.log(`[SERVER] Server is running on port ${process.env.APP_PORT}`);
 });
