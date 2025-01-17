75d03365614177f3da32234f939c3c572fb03eee
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2025-01-17 14:19:25 +0530

    Update route.ts

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import { roleController } from "../controller/role.create.controller"; // Correct the import based on your export method
 import { asyncHandler } from "../middleware/asyncHandler";
+import { rolePermissionController } from "../controller/permission.role.create.controller";
+import { assignPermissionToRole } from "../controller/assign.role.permissions";
 
 const routes = Router();
 
 const PATH = {
   ADD: "/add",
+  ASSIGN:"/assign",
   CREATE: "/create",
   UPDATE: "/update/:id",
   DELETE: "/delete/:id",
 };
 
 routes.post(`${PATH.ADD}/addRole`,asyncHandler(roleController))
+routes.post(`${PATH.ADD}/addRolePermission`,asyncHandler(rolePermissionController))
+routes.post(`${PATH.ASSIGN}/assign-permissions`, asyncHandler(assignPermissionToRole))
 
 export default routes;
