6e42936674e459a8c6ac155b9a4f50d97a20daab
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2025-01-23 21:53:51 +0530

    Update generate.employee.id.ts
    - implemented dynamic model name

--- src/controller/generate.employee.id.ts
+++ src/controller/generate.employee.id.ts
@@ -1,17 +1,17 @@
 import { Counter } from "../model/employee.id.counter.schma";
 
-export const getNextEmployeeId = async (): Promise<String> => {
+export const getNextEmployeeId = async (schemaName:String): Promise<String> => {
   const counter = await Counter.findOneAndUpdate(
     {
-      modeName: "Employee",
+      modelName: `${schemaName}`,
     },
     {
       $inc: { seq: 1 },
     },
     {
       new: true,
-      upsert: true,
+      upsert: true
     }
   );
-  return ""
+  return counter ? `EMP${counter.seq}`:"EMP"
 };
