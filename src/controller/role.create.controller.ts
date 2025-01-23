a719464c377acc9731e872b20d3587cc0adb9fb3
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2025-01-23 19:33:23 +0530

    Update role.create.controller.ts
    -implemented role_id for time,date

--- src/controller/role.create.controller.ts
+++ src/controller/role.create.controller.ts
@@ -1,21 +1,28 @@
 import { Request, Response } from "express";
-import {Role} from "../model/role.schema"; 
+import { Role } from "../model/role.schema";
+import { v4 as uuidv4 } from "uuid";
 
-
-export const roleController =  async (req: Request, res: Response): Promise<Response  | void> => {
+export const roleController = async (
+  req: Request,
+  res: Response
+): Promise<Response | void> => {
   try {
     const { role_name, description } = req.body;
-    
+
     // Check if the role already exists
     const existingRole = await Role.findOne({ role_name });
     if (existingRole) {
-     return res.status(409).json({
+      return res.status(409).json({
         json: `${role_name} role already exists`,
       });
-      
     }
-    // If the role doesn't exist, create it
+
+    const d = new Date();
+    const dateString = `${d.getDate()}${d.getMonth() + 1}${d.getFullYear()}`; // Format as DD-MM-YYYY
+
+    const role_id = `${role_name}_${uuidv4().substring(0, 4)}_${dateString}`;
     const newRole = new Role({
+      role_id,
       role_name,
       description,
     });
@@ -23,7 +30,7 @@ export const roleController =  async (req: Request, res: Response): Promise<Resp
     await newRole.save();
 
     return res.status(201).json({
-      json : `${role_name} Role created successfully`,
+      json: `${role_name} Role created successfully`,
       data: newRole,
     });
   } catch (error) {
@@ -32,4 +39,4 @@ export const roleController =  async (req: Request, res: Response): Promise<Resp
       error: "An error occurred while saving the role",
     });
   }
-}
+};
