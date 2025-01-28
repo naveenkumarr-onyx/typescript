bf2136ecaf43e536993f85fa0032c8fb1b4b704c
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2025-01-28 23:33:29 +0530

    created JWT

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -4,11 +4,14 @@ import { asyncHandler } from "../middleware/asyncHandler";
 import { rolePermissionController } from "../controller/permission.role.create.controller";
 import { assignPermissionToRole } from "../controller/assign.role.permissions";
 import { employeeRegisterController } from "../controller/employee.register.controller";
+import { employeeLoginController } from "../controller/employee.login.controller";
+import { jwtMiddleware } from "../middleware/jwt";
 
 const routes = Router();
 
 const PATH = {
   ADD: "/add",
+  LOGIN:"/login",
   ASSIGN:"/assign",
   CREATE: "/create",
   UPDATE: "/update/:id",
@@ -19,5 +22,6 @@ routes.post(`${PATH.ADD}/addRole`,asyncHandler(roleController))
 routes.post(`${PATH.ADD}/addRolePermission`,asyncHandler(rolePermissionController))
 routes.post(`${PATH.ASSIGN}/assign-permissions`, asyncHandler(assignPermissionToRole))
 routes.post(`${PATH.ADD}/register-employee`, asyncHandler(employeeRegisterController))
+routes.get(`${PATH.LOGIN}/login-employee`,asyncHandler(employeeLoginController), jwtMiddleware)
 
 export default routes;
