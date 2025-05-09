e5d6af4051da9493cbc9261d4be8f109fe6e41b6
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2025-05-09 11:19:55 +0530

    modified DB

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import 'dotenv/config'
 import cors from "cors";
-import "./config/db";
+require ("./config/db");
 import routes from "./routes/route";
 
 
